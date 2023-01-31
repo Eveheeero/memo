014c228548688c83bc07b6032dcbf5a1f69b7e1c
Eveheeero <xhve00000@naver.com>
2023-01-31 13:19:37 +0000

    +

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,34 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
 module Main (main) where
 
-import qualified Brick
+import Brick ((<=>))
+import Brick qualified
+import Brick.Widgets.Border qualified
+import Brick.Widgets.Center qualified
 import Lib ()
 
-ui :: Brick.Widget ()
-ui = Brick.str "Hello World"
+str :: String -> Brick.Widget ()
+str = Brick.str
+
+vBox :: [Brick.Widget n] -> Brick.Widget n
+vBox = Brick.vBox
+
+hBox :: [Brick.Widget n] -> Brick.Widget n
+hBox = Brick.hBox
+
+out :: Ord n => Brick.Widget n -> s -> IO s
+out widget status = do
+  let wrapped = (Brick.Widgets.Border.border . center) widget
+  let app = Brick.simpleApp wrapped
+  Brick.defaultMain app status
+
+center :: Brick.Widget n -> Brick.Widget n
+center = Brick.Widgets.Center.center
 
 main :: IO ()
 main =
   do
-    Brick.simpleMain ui
-    return ()
+    let left_menu = (Brick.Widgets.Border.border . vBox) [str "Hello 1", str "Hello 2"]
+    let frame = hBox [left_menu, str "Hello 3"]
+    out frame ()
